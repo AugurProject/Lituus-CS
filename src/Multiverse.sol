ae22796d7f30ea35d0409176989ea0c2137b2e6f
Tanya Bushenyova <viatrix.x@gmail.com>
2026-05-31 23:36:53 +0300

    Add interfaces and constructor

--- src/Multiverse.sol
+++ src/Multiverse.sol
@@ -6,6 +6,8 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import { IZoltar } from "./interfaces/IZoltar.sol";
 import { ILituusRep } from "./interfaces/ILituusRep.sol";
+import { LituusRep } from "./LituusRep.sol";
+import { IReputationToken } from "./interfaces/IReputationToken.sol";
 
 contract Multiverse {
 
@@ -59,24 +61,36 @@ contract Multiverse {
 
     error ZeroAddress();
 
-    constructor(IZoltar _zoltar) {
+    constructor(IZoltar _zoltar, uint248 _initialZoltarUniverseId) {
         ZOLTAR = _zoltar;
         if (address(ZOLTAR) == address(0)) revert ZeroAddress();
 
-        // TODO: Get the rep token address from the genesis universe in Zoltar
-        // Deploy a Lituus REP token that wraps the Zoltar REP token
-        // ILituusRep repToken = new LituusRep(address(0)); // Pass address(0) for now, will set the correct address after deployment
+        // get the rep token address from the initial universe in Zoltar
+        IReputationToken initialZoltarRepToken = ZOLTAR.getRepToken(_initialZoltarUniverseId);
+        // deploy a Lituus REP token that wraps the Zoltar REP token
+        // token symbol will use universe.history as a suffix. Genesis universe will have symbol "REP0"
+        ILituusRep repToken = new LituusRep(address(this), address(initialZoltarRepToken), "Lituus Reputation Token", "REP0");
 
         Universe memory genesisUniverse;
         genesisUniverse.favoriteChild = 0;
         genesisUniverse.parent = 0;
-        // TODO genesisUniverse.repToken = repToken;
+        genesisUniverse.repToken = repToken;
         genesisUniverse.forkState = 0;
         genesisUniverse.heir = 0;
         genesisUniverse.history = new bytes32[](0);
         genesisUniverse.forkQuery = 0;
-        genesisUniverse.supplyBeforeFork = genesisUniverse.repToken.totalSupply();
+        genesisUniverse.supplyBeforeFork = repToken.totalSupply(); // TODO: what should it be?
         genesisUniverse.queryTokenizer = address(0);
         universes[0] = genesisUniverse;
+    }
+
+    function wrap(uint248 universeId, uint256 amount) external {
+        // TODO: check universe status
+        universes[universeId].repToken.wrap(msg.sender, amount);
+    }
+
+    function unwrap(uint248 universeId, uint256 amount) external {
+        // TODO: check universe status
+        universes[universeId].repToken.unwrap(msg.sender, amount);
     }
 }
--- src/interfaces/ILituusRep.sol
+++ src/interfaces/ILituusRep.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.35;
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface ILituusRep is IERC20 {
-    function wrap(uint256 amount) external;
-    function unwrap(uint256 amount) external;
+    function wrap(address sender, uint256 amount) external;
+    function unwrap(address sender, uint256 amount) external;
 }
